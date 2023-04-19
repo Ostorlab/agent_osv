302dcff3bfe6b4a365904d7022dc1dc7ec56e923
oussama <o.essaji@ostorlab.dev>
2023-04-19 11:05:40 +0000

    OSV WRAPPER.

--- agent/osv_wrapper.py
+++ agent/osv_wrapper.py
@@ -1,7 +1,10 @@
 """OSV Wrapper responsible for running OSV Scanner on the appropriate file"""
 import logging
+import mimetypes
+import os
 from typing import Optional
 
+import magic
 from rich import logging as rich_logging
 
 logging.basicConfig(
@@ -25,13 +28,51 @@ class OSVWrapper:
         self.extension: str | None = ""
 
     def is_valid_file(self) -> bool:
-        raise NotImplementedError
+        """check whether the file is valid lock file or not
+        Args:
+            content: the file content
+        Returns:
+            Boolean whether the file is valid
+        """
+        if self.content is None or self.content == b"":
+            logger.error("Received empty content.")
+            return False
+
+        self.extension = self.get_file_type()
+        if self.extension not in LOCK_FILES_EXTENSIONS:
+            logger.error("This type of file not supported.")
+            return False
+
+        return True
 
     def get_file_type(self) -> str | None:
-        raise NotImplementedError
+        """Get the file extension
+        Args:
+        Returns:
+            The file extension
+        """
+        if self.path is not None and len(os.path.splitext(self.path)[1]) >= 2:
+            return os.path.splitext(self.path)[1]
+        if self.content is not None:
+            mime = magic.from_buffer(self.content, mime=True)
+            return mimetypes.guess_extension(mime)
+        return None
 
     def write_content_to_file(self) -> str | None:
-        raise NotImplementedError
+        """Write the file content to a file
+        Args:
+        Returns:
+            The file path
+        """
+        if self.content is None or self.content != b"":
+            return None
+
+        decoded_content = self.content.decode("utf-8")
+        logger.info("null path")
+        file_path = f"/tmp/lock_file{self.extension}"
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(decoded_content)
+        return file_path
 
     def build_putput(self, output: Optional[bytes]) -> None:
         raise NotImplementedError
