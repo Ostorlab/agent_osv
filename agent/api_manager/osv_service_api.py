e654da05b2e73f07e835bc61dbdf3287ed188e05
Mohamed Elyousfi <mohamed.elyousfi@ostorlab.dev>
2024-02-02 17:56:48 +0100

    Test file

--- agent/api_manager/osv_service_api.py
+++ agent/api_manager/osv_service_api.py
@@ -40,7 +40,7 @@ class VulnData:
     else None,
 )
 def query_osv_api(
-    package_name: str, version: str, ecosystem: str | None
+    package_name: str, version: str, ecosystem: str | None = None
 ) -> dict[str, Any] | None:
     """Query the OSV API with the specified version, package name, and ecosystem.
     Args:
